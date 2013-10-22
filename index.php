fee2d27a4e7c45a49c993fef15b9a7afd2dfa54b
Oliver Kinne <oliver_kinne@hotmail.com>
2013-10-22 20:22:21 +0100

    Initial Commit
    The homepage core structure, yet to be built properly. I am focussing on
    the database creation first, before worrying about the website.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html xmlns:fb="https://www.facebook.com/2008/fbml" xmlns:addthis="http://www.addthis.com/help/api-spec" xmlns:og="http://ogp.me/ns#">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+	<meta name="viewport" content="width=device-width" />
+	<meta name="HandheldFriendly" content="true" />
+	<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+
+	<title>Clans of Macaria - Welcome</title>
+	<meta name="description" content="The online home of Clans of Macaria - a turn-based game of exploring, building settlements and growing clans from generation to generation." />
+	<meta name="keywords" content="board,game,turn based,players,rounds,settle,clan,team" />
+
+	<meta property="og:site_name" content="Clans of Macaria" />
+	<meta property="og:title" content="Clans of Macaria - Welcome" />
+	<meta property="og:description" content="The online home of Clans of Macaria - a turn-based game of exploring, building settlements and growing clans from generation to generation." />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="http://www.oliverkine.com/clans-of-macaria/" />
+</head>
+
+<body>
+	<p>To be built...</p>
+</body>
+
+</html>
