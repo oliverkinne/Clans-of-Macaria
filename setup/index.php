596e95177d5025d127ec7fe63d4a22800584a066
Oliver Kinne <oliver_kinne@hotmail.com>
2013-10-23 21:47:52 +0100

    Extended the setup to create some rudimentary tables and the "clans" MySQL user with a password entered by the user

--- setup/index.php
+++ setup/index.php
@@ -1,4 +1,9 @@
 <?php include '/srv/www/htdocs/clans-of-macaria/includes/header.php' ?>
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+?>
 
 	<title>Clans of Macaria - Setup</title>
 	<meta property="og:title" content="Clans of Macaria - Setup" />
@@ -28,7 +33,8 @@ switch ($_GET['setup']) {
 	<p>YOU WILL LOSE ALL DATA AND EVERYTHING WILL BE SET UP AGAIN FROM SCRATCH!!</p>
 	<p>Only click SETUP below if you definitely want to set up everything from scratch and are happy to loose all data.</p>
 	<form action="." method="get">
-		<p>Enter database root user password: <input type="text" name="root"></p>
+		<p>Enter MySQL root user password: <input type="text" name="root"></p>
+		<p>Choose Clans' database user password: <input type="text" name="clans"></p>
 		<p><input type="hidden" name="setup" value="confirm"><input type="submit" name="submit" value="SETUP"></p>
 	</form>
 <?php
@@ -41,57 +47,116 @@ switch ($_GET['setup']) {
 <?php
 		$con = mysqli_connect('localhost', 'root', $_GET['root']);
 		if (mysqli_connect_errno())
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			echo '<p>Failed to connect to MySQL: ' . mysqli_connect_error() . '</p>';
 		else {
-			$sql="DROP USER clans@localhost";
+			// Database
+			$sql = 'DROP DATABASE clans_of_macaria';
 			if (mysqli_query($con, $sql))
-				echo "User clans deleted successfully";
+				echo '<p>Database clans_of_macaria deleted successfully</p>';
 			else
-				echo "Error deleting user clans: " . mysqli_error($con);
+				echo '<p>Error deleting database: ' . mysqli_error($con) . '</p>';
 
-			$sql="CREATE USER clans@localhost IDENTIFIED BY 'honeypot'";
+			$sql = 'CREATE DATABASE clans_of_macaria';
 			if (mysqli_query($con, $sql))
-				echo "User clans created successfully";
+				echo '<p>Database clans_of_macaria created successfully</p>';
 			else
-				echo "Error creating user clans: " . mysqli_error($con);
+				echo '<p>Error creating database: ' . mysqli_error($con) . '</p>';
 
-			$sql="DROP DATABASE clans_of_macaria";
-			if (mysqli_query($con, $sql))
-				echo "Database clans_of_macaria deleted successfully";
-			else
-				echo "Error deleting database: " . mysqli_error($con);
+			// Switch to new database
+			$sql = 'USE clans_of_macaria';
+			if (mysqli_query($con, $sql)) {
+				echo '<p>Database clans_of_macaria selected successfully</p>';
 
-			$sql="CREATE DATABASE clans_of_macaria";
-			if (mysqli_query($con, $sql))
-				echo "Database clans_of_macaria created successfully";
-			else
-				echo "Error creating database: " . mysqli_error($con);
+				// Tables
+				$sql = 'DROP TABLE games';
+				if (mysqli_query($con, $sql))
+					echo '<p>Table games deleted successfully</p>';
+				else
+					echo '<p>Error deleting table games: ' . mysqli_error($con) . '</p>';
 
-			$sql="GRANT ALL PRIVILEGES ON clans_of_macaria.* TO clans@localhost";
-			if (mysqli_query($con, $sql))
-				echo "User clans given full access to database clans_of_macaria successfully";
-			else
-				echo "Error giving full access: " . mysqli_error($con);
+				$sql = 'CREATE TABLE games (gameid BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(75), PRIMARY KEY (gameid))';
+				if (mysqli_query($con, $sql))
+					echo '<p>Table games created successfully</p>';
+				else
+					echo '<p>Error creating table: ' . mysqli_error($con) . '</p>';
 
-			$sql="FLUSH PRIVILEGES";
-			if (mysqli_query($con, $sql))
-				echo "Flushed privileges successfully";
+				$sql = 'DROP TABLE players';
+				if (mysqli_query($con, $sql))
+					echo '<p>Table players deleted successfully</p>';
+				else
+					echo '<p>Error deleting table players: ' . mysqli_error($con) . '</p>';
+
+				$sql = 'CREATE TABLE players (playerid BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(75), PRIMARY KEY (playerid))';
+				if (mysqli_query($con, $sql))
+					echo '<p>Table players created successfully</p>';
+				else
+					echo '<p>Error creating table players: ' . mysqli_error($con) . '</p>';
+
+				$sql = 'SHOW TABLES';
+				$result = mysqli_query($con, $sql);
+				if ($result) {
+					echo '<p>Tables created:</p><ul>';
+					while ($row = mysqli_fetch_row($result)) {
+		                echo '<li><p>' . $row[0] . '</p><ul>';
+
+						$sql = 'DESCRIBE ' . $row[0];
+						$details = mysqli_query($con, $sql);
+						if ($details) {
+							while ($detail = mysqli_fetch_row($details))
+				                echo '<li><p>' . $detail[0] . ' - ' . $detail[1] . ' - NULL=' . $detail[2] . ' - KEY=' . $detail[3] . ' - DEFAULT=' . $detail[4] . ' - ' . $detail[5] . '</p></li>';
+						}
+
+						echo '</ul></li>';
+					}
+        		    echo '</ul>';
+				}
+				else
+					echo '<p>Error listing tables: ' . mysqli_error($con) . '</p>';
+			}
 			else
-				echo "Error flushing privileges: " . mysqli_error($con);
+				echo '<p>Error selecting database clans_of_macaria: ' . mysqli_error($con) . '</p>';
+
+			// Write out clans database user's password
+			file_put_contents('access.php', '<' . '?php $clans_of_macaria_password = \'' . str_replace('\'', "\\'", $_GET['clans']) . '\'; ?' . '>');
+			echo '<p>User clans password saved to ' . getcwd() . '/access.php</p>';
+
+			// Clans database user
+			$sql = 'DROP USER clans';
+			if (mysqli_query($con, $sql))
+				echo '<p>User clans deleted successfully</p>';
+			else
+				echo '<p>Error deleting user clans: ' . mysqli_error($con) . '</p>';
+
+			$sql = 'CREATE USER clans IDENTIFIED BY \'' . str_replace('\'', "\\'", $_GET['clans']) . '\'';
+			if (mysqli_query($con, $sql))
+				echo '<p>User clans created successfully</p>';
+			else
+				echo '<p>Error creating user clans: ' . mysqli_error($con) . '</p>';
+
+			$sql = 'GRANT SELECT, INSERT, UPDATE, DELETE ON clans_of_macaria.* TO clans';
+			if (mysqli_query($con, $sql))
+				echo '<p>User clans given SELECT, INSERT, UPDATE and DELETE access to database clans_of_macaria successfully</p>';
+			else
+				echo '<p>Error giving full access: ' . mysqli_error($con) . '</p>';
+
+			$sql = 'FLUSH PRIVILEGES';
+			if (mysqli_query($con, $sql))
+				echo '<p>Flushed privileges successfully</p>';
+			else
+				echo '<p>Error flushing privileges: ' . mysqli_error($con) . '</p>';
+
+			$conclans = mysqli_connect('localhost', 'clans', $_GET['clans']);
+			if (mysqli_connect_errno())
+				echo '<p>Failed to connect to MySQL: ' . mysqli_connect_error() . '</p>';
+			else {
+				echo '<p>User clans connected successfully</p>';
+
+				mysqli_close($conclans);
+			}
+
 		}
 
-		mysql_close();
-
-		$con = mysqli_connect('localhost', 'clans', 'honeypot');
-		if (mysqli_connect_errno())
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		else {
-			$sql="CREATE TABLE games (name VARCHAR(255))";
-			if (mysqli_query($con, $sql))
-				echo "Table games created successfully";
-			else
-				echo "Error creating table: " . mysqli_error($con);
-		}
+		mysqli_close($con);
 
 		break;
 }
