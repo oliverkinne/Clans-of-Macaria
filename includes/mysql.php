ccd94f4c7d7fd2d721bb073265006d6604d786d8
Oliver Kinne <oliver_kinne@hotmail.com>
2013-11-01 20:33:49 +0000

    Added generic dosql function with optional echo reporting

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,8 +1,92 @@
 <?php
-function create_table($con, $table, $fields) {
-	if (mysqli_query($con, 'CREATE TABLE ' . $table . ' (' . $fields . ')'))
-		echo '<p>Table ' . $table . ' created successfully</p>';
-	else
-		echo '<p>Error creating table ' . $table . ': ' . mysqli_error($con) . '</p>';
+function dosql($con, $sql, $echo) {
+	$rv = mysqli_query($con, $sql);
+
+	if (!is_null($echo) && $echo) {
+		$sql = explode(' ', $sql, 2);
+
+		$object_type = '';
+		$object_name = '';
+		switch (strtoupper($sql[0])) {
+			case 'DROP':
+			case 'CREATE':
+				$object_type = explode(' ', $sql[1], 3);
+				$object_name = $object_type[1];
+				$object_type = strtolower($object_type[0]);
+				break;
+
+			case 'GRANT':
+				$object_type = 'privileges';
+				$object_name = explode(' ON ', $sql[1], 3);
+				$object_name[1] = explode(' ', $object_name[1], 2);
+				$object_name[1] = $object_name[1][0];
+				$object_name = $object_name[0] . ' on ' . $object_name[1];
+				break;
+
+			case 'FLUSH':
+				$object_type = strtolower($sql[1]);
+				$object_name = '';
+				break;
+
+			case 'INSERT':
+				$object_type = 'records into';
+				$object_name = explode(' ', $sql[1], 3);
+				$object_name = $object_name[1];
+				break;
+
+			case 'DELETE':
+				$object_type = 'records from';
+				$object_name = explode(' ', $sql[1], 3);
+				$object_name = $object_name[1];
+				break;
+
+			case 'USE':
+				$object_type = 'database';
+				$object_name = $sql[1];
+				break;
+		}
+
+		$command = strtolower($sql[0]);
+		switch (strtoupper($sql[0])) {
+			case 'DROP':
+				$command = 'dropping';
+				break;
+
+			case 'CREATE':
+				$command = 'creating';
+				break;
+
+			case 'GRANT':
+				$command = 'Granting';
+				break;
+
+			case 'FLUSH':
+				$command = 'flushing';
+				break;
+
+			case 'INSERT':
+				$command = 'inserting';
+				break;
+
+			case 'DELETE':
+				$command = 'deleting';
+				break;
+
+			case 'USE':
+				$command = 'switching to';
+				break;
+		}
+		
+		if ($rv)
+			echo '<p>' . strtoupper(substr($command, 0, 1)) . substr($command, 1) . ' ' . $object_type . ' ' . $object_name . ' successful.</p>';
+		else
+			echo '<p>Error ' . $command . ' ' . $object_type . ' ' . $object_name . ': ' . mysqli_error($con) . '</p>';
+	}
+
+	return $rv;
+}
+
+function create_table($con, $table, $fields, $echo) {
+	dosql($con, 'CREATE TABLE ' . $table . ' (' . $fields . ')', $echo);
 }
 ?>
